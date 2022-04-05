6086a3991287d4f092c170289057f8e694a8cac5
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-04-04 19:19:44 -0700

    Created empty component files

---
+++ src/Components/ButtonElement.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+background: none;
+font-size: 16px;
+margin: 5px;
+color: white;
+padding: 4px 8px;
+border: 2px solid white;
+border-radius: 3px;
+display: block;
+text-decoration: none;
+font: inherit;
+cursor: pointer;
+
+
+:hover{
+    -webkit-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1);
+    border: 3px solid #B8AAB9;
+    color: #B8AAB9;
+    transition: 200 ease-in-out;
+  }
+`
