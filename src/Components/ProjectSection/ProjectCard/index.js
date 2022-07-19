a1d5d473baceceab3e22321f0021c12a93222041
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-07-19 13:05:40 -0500

    Updated ProjectCard folder in ProjectSection folder so it only renders the GitHub Repo button if the project does not have a deployed application

--- src/Components/ProjectSection/ProjectCard/index.js
+++ src/Components/ProjectSection/ProjectCard/index.js
@@ -1,28 +1,58 @@
-import React from 'react'
-import {Card, ImageWrapper, DescriptionWrapper, HeaderSection, MainTitle, SubTitle, Content, Image, ButtonWrapper} from './CardElements'
-import { Button } from '../../ButtonElement'
-export const ProjectCard = ({img,title,description, line,alt, website, repo}) => {
+import React from "react";
+import {
+  Card,
+  ImageWrapper,
+  DescriptionWrapper,
+  HeaderSection,
+  MainTitle,
+  SubTitle,
+  Content,
+  Image,
+  ButtonWrapper,
+} from "./CardElements";
+import { Button } from "../../ButtonElement";
+export const ProjectCard = ({
+  img,
+  title,
+  description,
+  line,
+  alt,
+  website,
+  repo,
+}) => {
+  console.log({ website });
+  let web = website;
+  console.log(web);
   return (
     <Card>
-    <ImageWrapper>
-        <Image src={img} alt={alt}/>
+      <ImageWrapper>
+        <Image src={img} alt={alt} />
         {/* <OverlayDescription>Technologies: {description}</OverlayDescription>
         <OverlayTitle>{title}</OverlayTitle> */}
-    </ImageWrapper>
-    <DescriptionWrapper>
-    <HeaderSection>
-        <MainTitle>{title}</MainTitle>
-        <SubTitle>Technologies: {description}</SubTitle>
-        <Content>{line}</Content>
-
-      </HeaderSection>
+      </ImageWrapper>
+      <DescriptionWrapper>
+        <HeaderSection>
+          <MainTitle>{title}</MainTitle>
+          <SubTitle>Technologies: {description}</SubTitle>
+          <Content>{line}</Content>
+        </HeaderSection>
         <ButtonWrapper>
-            <Button as='a' href={website} target='_blank'>Live App</Button>
-            <Button as='a' href={repo} target='_blank'>Github Repo</Button>
-  
+          {website === repo ? (
+            <Button as="a" href={repo} target="_blank">
+              Github Repo
+            </Button>
+          ) : (
+            <>
+              <Button as="a" href={website} target="_blank">
+                Live App
+              </Button>
+              <Button as="a" href={repo} target="_blank">
+                Github Repo
+              </Button>
+            </>
+          )}
         </ButtonWrapper>
-    </DescriptionWrapper>
-</Card>
-)
-}
-
+      </DescriptionWrapper>
+    </Card>
+  );
+};
